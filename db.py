67ddad51eb5a86b67614d0033365384b11c76620
hexlyai <hexlyai012@gmail.com>
2025-08-31 21:43:01 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 from supabase import create_client
 import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://mlfqfnfgoxovngxbwajm.supabase.co")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sZnFmbmZnb3hvdm5neGJ3YWptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NTA0NzEsImV4cCI6MjA3MjIyNjQ3MX0.DO1KtBg1RvG87n2a_KgOAQQx4CkIIPsmyplUqdj6Rtg")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def get_user(telegram_id):
